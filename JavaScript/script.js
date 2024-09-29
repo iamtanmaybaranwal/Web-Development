05bdc89c1cc8c49df2d541b52d53dbc3b08bdbc3
Tanmay Baranwal <157231264+iamtanmaybaranwal@users.noreply.github.com>
2024-09-29 10:07:21 +0530

    updated

---
+++ JavaScript/script.js
@@ -0,0 +1,17 @@
+alert("Hello World")
+
+console.log("Hello this is tanmay")
+console.log("How you are doing..??")
+console.log("This code is looking like a wow....!!")
+
+var a = prompt("Enter your number here..")
+console.log("Your number is "+a)
+
+var isTrue = confirm("Do you really want to exit and blast your computer?")
+
+if(isTrue){
+    console.log("You have svaed your computer from blasting")
+}
+else{
+    console.log("Blasting your system....")
+}
