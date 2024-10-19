9f08cd26b4b0aff0aa75bd0e3015f1660b140b70
Tanmay Baranwal <157231264+iamtanmaybaranwal@users.noreply.github.com>
2024-10-19 21:37:42 +0530

    updated

--- JavaScript/conditional.js
+++ JavaScript/conditional.js
@@ -2,18 +2,23 @@ console.log("Heyy this is a script for conditional statements.")
 
 let age = 7;
 let grace = 2;
-console.log(age + grace);
-console.log(age - grace);
-console.log(age * grace);
-console.log(age / grace);
-console.log(age ** grace);
-console.log(age % grace);
+
+// console.log(age + grace);
+// console.log(age - grace);
+// console.log(age * grace);
+// console.log(age / grace);
+// console.log(age ** grace);
+// console.log(age % grace);
 
 
 
-if((age+grace)>18){
-    console.log("You can Vote");
-}
-else{
-console.log("You cannot vote")
-}
+// if((age+grace)>18){
+//     console.log("You can Vote");
+// }
+// else{
+// console.log("You cannot vote")
+// }
+
+a = 5;
+b = 6;
+let c = a > b ?(a-b):(b-a);
